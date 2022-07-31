b5b246af30e8e72c5a223aad1b2aa88ffc303a2e
EmersonLopes <emersonsilvalopes1@gmail.com>
2022-07-31 14:07:03 -0300

    Principais Bibliotecas de Logging, Log Levels e Logging no Spring com Logback; Implementando Logging nos Microservices com Log4J2 e boas práticas; Conceitos de API Composition Pattern

--- authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -7,6 +7,9 @@ import com.ead.authuser.enums.UserType;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,10 +20,13 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
+@Log4j2
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/auth")
 public class AuthenticationController {
+
+//    Logger logger = LogManager.getLogger(AuthenticationController.class);
 
     @Autowired
     private UserService userService;
@@ -30,12 +36,15 @@ public class AuthenticationController {
             @RequestBody @Validated(UserDto.UserView.RegistrationPost.class)
             @JsonView(UserDto.UserView.RegistrationPost.class) UserDto userDto) {
 
+        log.debug("POST registerUser userDto received {} ", userDto.toString());
         if (userService.existsByUserName(userDto.getUserName())) {
+        log.warn("Username {} is Already Exists ", userDto.getUserName());
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Username is Already Taken!");
         }
 
         if (userService.existsByEmail(userDto.getEmail())) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Email is Already Taken!");
+        log.warn("Email {} is Already Exists ", userDto.getUserName());
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Email is Already!");
         }
 
         var userModel = new UserModel();
@@ -47,9 +56,20 @@ public class AuthenticationController {
 
         userService.save(userModel);
 
+        log.debug("POST registerUser userModel saved {} ", userModel.toString());
+        log.info("User saved successfully userId {} ", userModel.getUserId());
         return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
 
     }
 
+    @GetMapping("/")
+    public String index() {
+        log.trace("TRACE");
+        log.debug("DEBUG");
+        log.info("INFO");
+        log.warn("WARN");
+        log.error("ERROR");
+        return "Logging Spring Boot...";
+    }
 
 }
--- authuser/src/main/java/com/ead/authuser/controllers/UserController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
 import com.ead.authuser.specifications.SpecificationTemplate;
 import com.fasterxml.jackson.annotation.JsonView;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -23,6 +24,7 @@ import java.util.UUID;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+@Log4j2
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/users")
@@ -72,8 +74,10 @@ public class UserController {
             @RequestBody @Validated(UserDto.UserView.UserPut.class)
             @JsonView(UserDto.UserView.UserPut.class) UserDto userDto) {
 
+        log.debug("PUT updateUser userDto {} ", userDto.toString());
         Optional<UserModel> userModelOptional = userService.findById(userId);
         if (!userModelOptional.isPresent()) {
+            log.warn("User {} not found, userId ", userDto.getUserId());
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
         } else {
             var userModel = userModelOptional.get();
@@ -82,6 +86,9 @@ public class UserController {
             userModel.setCpf(userDto.getCpf());
             userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
             userService.save(userModel);
+
+            log.debug("PUT updateUser userModel saved {} ", userModel.toString());
+            log.info("User saved successfully userId {} ", userModel.getUserId());
 
             return ResponseEntity.status(HttpStatus.OK).body("User updated successfuly.");
         }
@@ -98,6 +105,7 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
         }
         if (!userModelOptional.get().getPassword().equals(userDto.getOldPassword())) {
+            log.warn("Mismatched old password, userId ", userDto.getUserId());
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Mismatched old password");
 
         } else {
