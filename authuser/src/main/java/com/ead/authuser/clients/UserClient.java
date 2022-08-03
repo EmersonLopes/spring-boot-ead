75a2bfd16aaa1bca0740dfcdb8656048a206663f
EmersonLopes <emersonsilvalopes1@gmail.com>
2022-08-03 19:51:15 -0300

    API Composition Pattern em Authuser Microservice – Parte 02

--- authuser/src/main/java/com/ead/authuser/clients/UserClient.java
+++ authuser/src/main/java/com/ead/authuser/clients/UserClient.java
@@ -1,10 +1,16 @@
 package com.ead.authuser.clients;
 
 import com.ead.authuser.dtos.CourseDto;
+import com.ead.authuser.dtos.ResponsePageDto;
+import com.ead.authuser.services.UtilsService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
@@ -19,17 +25,24 @@ public class UserClient {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private UtilsService utilsService;
+
     String REQUEST_URI = "http://localhost:8082";
 
     public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable) {
 
         List<CourseDto> searchResult = null;
-        String url = REQUEST_URI + "/courses?userId=" + userId + "&page=" + pageable.getPageNumber() + "&size="
-                + pageable.getPageSize() + "&sort=" + pageable.getSort().toString().replaceAll(": ", ",");
+        String url = utilsService.createUrl(userId, pageable);
 
         log.debug("Request URL: {}", url);
         log.info("Request URL: {}", url);
         try {
+            ParameterizedTypeReference<ResponsePageDto<CourseDto>> resposeType = new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {
+            };
+            ResponseEntity<ResponsePageDto<CourseDto>> result = restTemplate.exchange(url, HttpMethod.GET, null, resposeType);
+
+            searchResult = result.getBody().getContent();
 
 
             log.debug("Response NUmber of Elements: {}", searchResult.size());
@@ -39,7 +52,7 @@ public class UserClient {
         }
         log.info("Ending reques /courses userId {}", userId);
 
-        return ;
+        return new PageImpl<>(searchResult);
 
     }
 }
