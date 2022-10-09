0130ea112f34de6b3ff876ad6a6d976d7f257f87
EmersonLopes <emersonsilvalopes1@gmail.com>
2022-10-09 14:09:17 -0300

    Conceitos e Fluxo do Spring Security para Basic Authentication; Basic Authentication em AuthUser com Roles; Users Roles com Encoder Password em AuthUser

--- authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -2,9 +2,12 @@ package com.ead.authuser.controllers;
 
 
 import com.ead.authuser.dtos.UserDto;
+import com.ead.authuser.enums.RoleType;
 import com.ead.authuser.enums.UserStatus;
 import com.ead.authuser.enums.UserType;
+import com.ead.authuser.models.RoleModel;
 import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.RoleService;
 import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.extern.log4j.Log4j2;
@@ -14,6 +17,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,6 +35,12 @@ public class AuthenticationController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @PostMapping("/signup")
     public ResponseEntity<Object> registerUser(
             @RequestBody @Validated(UserDto.UserView.RegistrationPost.class)
@@ -38,14 +48,19 @@ public class AuthenticationController {
 
         log.debug("POST registerUser userDto received {} ", userDto.toString());
         if (userService.existsByUserName(userDto.getUserName())) {
-        log.warn("Username {} is Already Exists ", userDto.getUserName());
+            log.warn("Username {} is Already Exists ", userDto.getUserName());
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Username is Already Taken!");
         }
 
         if (userService.existsByEmail(userDto.getEmail())) {
-        log.warn("Email {} is Already Exists ", userDto.getUserName());
+            log.warn("Email {} is Already Exists ", userDto.getUserName());
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Email is Already!");
         }
+
+        RoleModel roleModel = roleService.findByRoleName(RoleType.ROLE_STUDENT)
+                .orElseThrow(() -> new RuntimeException("Error: Role is Not Found"));
+
+        userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
 
         var userModel = new UserModel();
         BeanUtils.copyProperties(userDto, userModel);
@@ -53,6 +68,7 @@ public class AuthenticationController {
         userModel.setUserType(UserType.STUDENT);
         userModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
         userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+        userModel.getRoles().add(roleModel);
 
         userService.save(userModel);
 
